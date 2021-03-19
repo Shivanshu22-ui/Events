b38e7abbe9c6b324dac609aa52b0870b3d6df0ee
Shivanshu22-ui <shivasingh310102@gmail.com>
2021-03-19 19:37:01 +0530

    sections addition

--- script.js
+++ script.js
@@ -20,18 +20,18 @@ $('#fullpage').fullpage({
   navigation: true,
   slidesNavigation: true,
   controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection','sixthsection','seventhsection', 'fifthSection'],
   menu: '#menu',
   afterLoad: function(anchorLink, index) {
     $header_top.css('background', 'rgba(0, 47, 77, .3)');
     $nav.css('background', 'rgba(0, 47, 77, .25)');
-    if (index == 5) {
+    if (index == 8) {
         $('#fp-nav').hide();
       }
   },
 
   onLeave: function(index, nextIndex, direction) {
-    if(index == 5) {
+    if(index == 8) {
       $('#fp-nav').show();
     }
   },
